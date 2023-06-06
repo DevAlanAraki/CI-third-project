622f9f70b04635fc845c8a8e92e22b631096e03b
Alan Tioshi Araki <alantioshiaraki@gmail.com>
2023-06-06 10:30:43 +0000

    Add game stage attempts

--- app.py
+++ app.py
@@ -1,2 +1,91 @@
 from words import words
 import random
+
+# Select the word
+
+
+def select_word():
+    word = random.choice(words)
+    return word.upper()
+
+# Game status (body)
+
+
+def display_hangman(attempts):
+    stages = [  # Game Over
+        """
+                  --------
+                  |      |
+                  |      O
+                  |     \\|/
+                  |      |
+                  |     / \\
+                  -
+              """,
+        # Last attempt
+        """
+                  --------
+                  |      |
+                  |      O
+                  |     \\|/
+                  |      |
+                  |     / 
+                  -
+              """,
+        # Remain 2 attempts
+        """
+                  --------
+                  |      |
+                  |      O
+                  |     \\|/
+                  |      |
+                  |      
+                  -
+              """,
+        # Remain 3 attempts
+        """
+                  --------
+                  |      |
+                  |      O
+                  |     \\|
+                  |      |
+                  |     
+                  -
+              """,
+        # Remain 4 attempts
+        """
+                  --------
+                  |      |
+                  |      O
+                  |      |
+                  |      |
+                  |     
+                  -
+              """,
+        # Remain 5 attempts
+        """
+                  --------
+                  |      |
+                  |      O
+                  |    
+                  |      
+                  |     
+                  -
+              """,
+        # First attempt
+        """
+                  --------
+                  |      |
+                  |      
+                  |    
+                  |      
+                  |     
+                  -
+              """
+    ]
+    
+    return stages[attempts]
+
+test = select_word()
+print(test)
+
