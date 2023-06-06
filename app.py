154fa55f9d4fb67898895da5cff5f8a07f07f0ab
Alan Tioshi Araki <alantioshiaraki@gmail.com>
2023-06-06 10:35:37 +0000

    Defined variables that are used in the game

--- app.py
+++ app.py
@@ -8,9 +8,21 @@ def select_word():
     word = random.choice(words)
     return word.upper()
 
+def play(word):
+    #Define variables
+    word_to_be_discovered = "_" * len(word) # _ _ _ _ _
+    guessed = False
+    letter_used = []
+    words_used = []
+    attempts = 6
+    
+    print(word)
+    print(word_to_be_discovered)
+
+word = select_word()
+play(word)
+
 # Game status (body)
-
-
 def display_hangman(attempts):
     stages = [  # Game Over
         """
@@ -89,3 +101,5 @@ def display_hangman(attempts):
 test = select_word()
 print(test)
 
+test_stages = display_hangman(4)
+print(test_stages)
